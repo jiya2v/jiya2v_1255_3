faa62ae0c945be9f19d7731f1e897e77aa34785a
jiya2v <jiya2v@gmail.com>
2023-10-06 10:24:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import {HashRouter, Routes, Route} from "react-router-dom";
+import { Home, About } from './components/F2';
+import Nav from './components/Nav';
+import Contact from './components/Contact';
+import EmployeeDetails from './components/EmployeeDetails';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <F1 /> */}
+      {/* <HashRouter>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </HashRouter> */}
+      {/*
+      To install: npm install react-router-dom
+
+      http://localhost:3000/#/  -> Home
+      http://localhost:3000/#/about -> About
+      */}
+      <EmployeeDetails />
     </div>
   );
 }
